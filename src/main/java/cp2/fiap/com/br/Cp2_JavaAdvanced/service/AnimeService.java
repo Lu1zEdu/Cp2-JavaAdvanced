dbd81b524f65dd27e6a0cc0d18b38067a0562f3a
Lu1zEdu <le6269199@gmail.com>
2025-04-08 16:58:42 -0300

    Para mim já deu de java por hoje

--- src/main/java/cp2/fiap/com/br/Cp2_JavaAdvanced/service/AnimeService.java
+++ src/main/java/cp2/fiap/com/br/Cp2_JavaAdvanced/service/AnimeService.java
@@ -1,34 +1,56 @@
 package cp2.fiap.com.br.Cp2_JavaAdvanced.service;
 
+import cp2.fiap.com.br.Cp2_JavaAdvanced.dto.AnimeRequest;
 import cp2.fiap.com.br.Cp2_JavaAdvanced.dto.AnimeResponse;
 import cp2.fiap.com.br.Cp2_JavaAdvanced.models.Anime;
 import cp2.fiap.com.br.Cp2_JavaAdvanced.repository.AnimeRepository;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class AnimeService {
-    private final AnimeRepository animeRepository;
 
-    public AnimeService(AnimeRepository animeRepository){
-        this.animeRepository = animeRepository;
+    @Autowired
+    private AnimeRepository animeRepository;
+
+    public AnimeResponse saveAnime(AnimeRequest request) {
+        Anime anime = animeToEntity(request);
+        Anime animeSalvo = animeRepository.save(anime);
+        return entityToResponse(animeSalvo);
     }
 
-    public List<AnimeResponse> animeToResponses(List<Anime> animes) {
-        List<AnimeResponse> animeResponses = new ArrayList<>();
-        for (Anime anime : animes) {
-            animeResponses.add(animeToResponse(anime));
-        }
-        return animeResponses;
+    public Page<AnimeResponse> findAll(Pageable pageable) {
+        return animeRepository.findAll(pageable).map(this::entityToResponse);
     }
 
-    public AnimeResponse animeToResponse(Anime anime) {
+    public Optional<Anime> findById(Long id) {
+        return animeRepository.findById(id);
+    }
+
+    public Anime animeToEntity(AnimeRequest dto) {
+        Anime anime = new Anime();
+        anime.setTitulo(dto.getTitulo());
+        anime.setDescricao(dto.getDescricao());
+        anime.setStudio(dto.getStudio());
+        anime.setStatus(dto.getStatus());
+        anime.setDataDeLancamento(dto.getDataDeLancamento());
+        anime.setDataDeTermino(dto.getDataDeTermino());
+        anime.setGeneroList(dto.getGeneroList());
+        return anime;
+    }
+
+    public AnimeResponse entityToResponse(Anime anime) {
         return new AnimeResponse(
                 anime.getId(),
                 anime.getTitulo(),
                 anime.getTemporada(),
                 anime.getDescricao(),
-                anime.getDataDeLançamento(),
+                anime.getDataDeLancamento(),
                 anime.getDataDeTermino(),
                 anime.getStudio(),
                 anime.getStatus(),
--- src/main/java/cp2/fiap/com/br/Cp2_JavaAdvanced/service/RankService.java
+++ src/main/java/cp2/fiap/com/br/Cp2_JavaAdvanced/service/RankService.java
@@ -1,4 +1,5 @@
 package cp2.fiap.com.br.Cp2_JavaAdvanced.service;
 
 public class RankService {
+
 }
